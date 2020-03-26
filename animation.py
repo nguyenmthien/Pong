47c2f7ab1f7488a13c8ca60f6d25348c64386870
nguyenmthien <nguyenmhien@live.com>
2020-03-26 21:57:05 +0700

    add main, convert indentation to spaces

--- animation.py
+++ animation.py
@@ -1,4 +1,4 @@
-"""Assets' animation and game logic
+"""Assets animation and game logic
 """
 
 import random
@@ -6,43 +6,44 @@ import pygame
 
 
 def ball_animation():
-	global ball_speed_x, ball_speed_y
-	
-	ball.x += ball_speed_x
-	ball.y += ball_speed_y
+    '''Animation and logic of ball'''
+    global ball_speed_x, ball_speed_y
+    
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
-	if ball.top <= 0 or ball.bottom >= screen_height:
-		ball_speed_y *= -1
-	if ball.left <= 0 or ball.right >= screen_width:
-		ball_start()
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_start()
 
-	if ball.colliderect(player) or ball.colliderect(opponent):
-		ball_speed_x *= -1
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
 
 def player_animation():
-	player.y += player_speed
+    player.y += player_speed
 
-	if player.top <= 0:
-		player.top = 0
-	if player.bottom >= screen_height:
-		player.bottom = screen_height
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
 
 def ball_start():
-	global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y
 
-	ball.center = (screen_width/2, screen_height/2)
-	ball_speed_y *= random.choice((1,-1))
-	ball_speed_x *= random.choice((1,-1))
+    ball.center = (screen_width/2, screen_height/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
 
 def opponent_ai():
-	if opponent.top < ball.y:
-		opponent.y += opponent_speed
-	if opponent.bottom > ball.y:
-		opponent.y -= opponent_speed
+    if opponent.top < ball.y:
+        opponent.y += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.y -= opponent_speed
 
-	if opponent.top <= 0:
-		opponent.top = 0
-	if opponent.bottom >= screen_height:
-		opponent.bottom = screen_height
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
 
 
---
+++ main.py
@@ -0,0 +1,7 @@
+"""Main game file
+"""
+
+import animation
+
+if __name__ == "__main__":
+    pass
