4f5c2363787a9177c1bb2acc62f45033fcfad7bf
nguyenmthien <nguyenmhien@live.com>
2020-03-26 21:46:21 +0700

    add animation.py

---
+++ animation.py
@@ -0,0 +1,48 @@
+"""Assets' animation and game logic
+"""
+
+import random
+import pygame
+
+
+def ball_animation():
+	global ball_speed_x, ball_speed_y
+	
+	ball.x += ball_speed_x
+	ball.y += ball_speed_y
+
+	if ball.top <= 0 or ball.bottom >= screen_height:
+		ball_speed_y *= -1
+	if ball.left <= 0 or ball.right >= screen_width:
+		ball_start()
+
+	if ball.colliderect(player) or ball.colliderect(opponent):
+		ball_speed_x *= -1
+
+def player_animation():
+	player.y += player_speed
+
+	if player.top <= 0:
+		player.top = 0
+	if player.bottom >= screen_height:
+		player.bottom = screen_height
+
+def ball_start():
+	global ball_speed_x, ball_speed_y
+
+	ball.center = (screen_width/2, screen_height/2)
+	ball_speed_y *= random.choice((1,-1))
+	ball_speed_x *= random.choice((1,-1))
+
+def opponent_ai():
+	if opponent.top < ball.y:
+		opponent.y += opponent_speed
+	if opponent.bottom > ball.y:
+		opponent.y -= opponent_speed
+
+	if opponent.top <= 0:
+		opponent.top = 0
+	if opponent.bottom >= screen_height:
+		opponent.bottom = screen_height
+
+
