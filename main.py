1a4d3e390eaffb560b0593a00b685ba2f272fffa
nguyenmthien <nguyenmhien@live.com>
2020-03-27 00:15:12 +0700

    change module names

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 import pygame
 import assets
 import animation
-import input
+import controls
 
 if __name__ == "__main__":
     assets.setup()
-    assets.draw_playing_field()
     while True:
-        input.update_input()
+        controls.update_input()
         animation.ball()
         animation.player()
         animation.opponent_ai()
