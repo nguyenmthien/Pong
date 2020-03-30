8a728b73de8ebf6929f381cd516b5391f648107a
nguyenmthien <nguyenmthien@live.com>
2020-03-30 21:35:29 +0700

    PEP8 for controls module

--- controls.py
+++ controls.py
@@ -6,88 +6,85 @@ import assets
 import ui
 import animation
 
+class Control:
+    """All controls for game"""
+    def __init__(self):
+        self.current_menu = "title screen"
 
-MENU = "title screen"
+    def game_input(self):
+        """Singleplayer input handler"""
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    assets.player_speed -= assets.player_control_speed
+                if event.key == pygame.K_DOWN:
+                    assets.player_speed += assets.player_control_speed
+                if event.key == pygame.K_ESCAPE:
+                    self.current_menu = "title screen"
+                    print("end single player")
+                    animation.reset()
 
-def game_input():
-    """Singleplayer input handler"""
-    global MENU
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    assets.player_speed += assets.player_control_speed
+                if event.key == pygame.K_DOWN:
+                    assets.player_speed -= assets.player_control_speed
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                assets.player_speed -= assets.player_control_speed
-            if event.key == pygame.K_DOWN:
-                assets.player_speed += assets.player_control_speed
-            if event.key == pygame.K_ESCAPE:
-                MENU = "title screen"
-                print("end single player")
-                animation.reset()
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                assets.player_speed += assets.player_control_speed
-            if event.key == pygame.K_DOWN:
-                assets.player_speed -= assets.player_control_speed
-
-
-def title_screen():
-    """Title screen input handler"""
-    global MENU
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                if ui.choice > 0:
-                    ui.choice -= 1
-            elif event.key == pygame.K_DOWN:
-                if ui.choice < 3:
-                    ui.choice += 1
-            if event.key == pygame.K_RETURN:
-                if ui.selection_list[ui.choice] == "SINGLE PLAYER":
-                    print("start single player")
-                    MENU = "single player"
-                if ui.selection_list[ui.choice] == "LOCAL MULTIPLAYER":
-                    print("start single player")
-                    MENU = "local multiplayer"
-                if ui.selection_list[ui.choice] == "QUIT":
-                    pygame.quit()
-                    sys.exit()
+    def title_screen(self):
+        """Title screen input handler"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    if ui.choice > 0:
+                        ui.choice -= 1
+                elif event.key == pygame.K_DOWN:
+                    if ui.choice < 3:
+                        ui.choice += 1
+                if event.key == pygame.K_RETURN:
+                    if ui.selection_list[ui.choice] == "SINGLE PLAYER":
+                        print("start single player")
+                        self.current_menu = "single player"
+                    if ui.selection_list[ui.choice] == "LOCAL MULTIPLAYER":
+                        print("start single player")
+                        self.current_menu = "local multiplayer"
+                    if ui.selection_list[ui.choice] == "QUIT":
+                        pygame.quit()
+                        sys.exit()
 
 
-def local_multiplayer():
-    """Local multiplayer inputs handler"""
-    global MENU
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                assets.player_speed -= assets.player_control_speed
-            if event.key == pygame.K_DOWN:
-                assets.player_speed += assets.player_control_speed
-            if event.key == pygame.K_w:
-                assets.opponent_speed -= assets.opponent_control_speed
-            if event.key == pygame.K_s:
-                assets.opponent_speed += assets.opponent_control_speed
-            if event.key == pygame.K_ESCAPE:
-                MENU = "title screen"
-                print("end local multiplayer")
-                animation.reset()
+    def local_multiplayer(self):
+        """Local multiplayer inputs handler"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    assets.player_speed -= assets.player_control_speed
+                if event.key == pygame.K_DOWN:
+                    assets.player_speed += assets.player_control_speed
+                if event.key == pygame.K_w:
+                    assets.opponent_speed -= assets.opponent_control_speed
+                if event.key == pygame.K_s:
+                    assets.opponent_speed += assets.opponent_control_speed
+                if event.key == pygame.K_ESCAPE:
+                    self.current_menu = "title screen"
+                    print("end local multiplayer")
+                    animation.reset()
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                assets.player_speed += assets.player_control_speed
-            if event.key == pygame.K_DOWN:
-                assets.player_speed -= assets.player_control_speed
-            if event.key == pygame.K_w:
-                assets.opponent_speed += assets.opponent_control_speed
-            if event.key == pygame.K_s:
-                assets.opponent_speed -= assets.opponent_control_speed
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    assets.player_speed += assets.player_control_speed
+                if event.key == pygame.K_DOWN:
+                    assets.player_speed -= assets.player_control_speed
+                if event.key == pygame.K_w:
+                    assets.opponent_speed += assets.opponent_control_speed
+                if event.key == pygame.K_s:
+                    assets.opponent_speed -= assets.opponent_control_speed
--- main.py
+++ main.py
@@ -9,19 +9,24 @@ import ui
 if __name__ == "__main__":
     ui.initialize_title_screen()
     assets.setup()
+    CONTROL = controls.Control()
     while True:
-        while controls.MENU == "title screen":
-            controls.title_screen()
+        while CONTROL.current_menu == "title screen":
+            CONTROL.title_screen()
             ui.main_menu()
-        while controls.MENU == "single player":
-            controls.game_input()
+        while CONTROL.current_menu == "single player":
+            CONTROL.game_input()
             animation.ball()
             animation.player()
             animation.opponent_ai()
             assets.draw_playing_field()
-        while controls.MENU == "local multiplayer":
-            controls.local_multiplayer()
+        while CONTROL.current_menu == "local multiplayer":
+            CONTROL.local_multiplayer()
             animation.ball()
             animation.player()
             animation.opponent()
             assets.draw_playing_field()
+        while CONTROL.current_menu == "local network server":
+            pass
+        while CONTROL.current_menu == "local network client":
+            pass
