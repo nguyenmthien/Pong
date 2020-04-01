6e0d263722165a1f5689569a4ea1fef41ae86e7e
KLan98 <49123155+KLan98@users.noreply.github.com>
2020-04-01 20:48:27 +0700

    import threading

--- networking.py
+++ networking.py
@@ -1,72 +1,96 @@
 import socket
-from _thread import *
+import controls
+import assets
+import threading
 
-host = "192.168.1.5"  # local IPv4 address
+Host = socket.gethostbyname(socket.gethostname())
 reply = ""
-port = 2033
-clients = 2
-clients_count = 0
+Port = 2033
+Port_client = 2056
+Is_game_running = False
+Check_bind = False
 
 
 class Server():
     def init_server(self):
+        global Check_bind
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # socket declaration
-        self.bind = self.s.bind((host, port))  # bind host and port
-        print("socket binded to port", port)
+        self.bind = self.s.bind((Host, Port))  # bind host and port
+        Check_bind = True
+        print("Bind status", Check_bind)
+        print("socket binded to port", Port)
 
-        self.s.listen(clients)  # allow upto 2 clients connection
+        self.s.listen(1)  # allow upto 1 client connection
         print("waiting for connection")
 
-    def threaded_client(self, connect):
-        """Threaded function"""
+    def threaded_client(self, clientsocket):
         # connect.send(str.encode("connected"))
-        self.data = connect.recv(2048)  # allow receiving 2048 bits data
+        self.data = clientsocket.recv(2048)  # allow receiving 2048 bits data
         self.reply = self.data.decode("utf-8")  # utf-8 encoding
         if not self.data:  # if sending incompleted data
             print("disconnected")
         else:
             print("received", self.reply)
-        connect.sendall(str.encode(self.reply))  # send reply back to client
-        # connect.close()
+        clientsocket.sendall(str.encode(self.reply))
+        # send reply back to client
 
     def sending_client(self):
-        """connection function"""
-        self.connect, self.address = self.s.accept()
+        """client handle"""
+        self.clientsocket, self.address = self.s.accept()
         print("client's address:", self.address)
-        self.clients_count += 1
-        self.connect.send("from server: speaking".encode())
+        # print out client's address
+
+        self.clientsocket.send("from server: speaking".encode())
         # send data to client
-        start_new_thread(self.threaded_client, (self.connect, ))
+
+        threading.Thread(target=self.threaded_client,
+                         args=(self.clientsocket, )).start()
         # start new thread after sending message to client
-        return self.clients_count
 
 
-class Network:
+class Client:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.host = socket.gethostbyname(socket.gethostname())
+        # self.port = 2033
         self.address = (self.host, self.port)
         self.id = self.connect()
 
     def connect(self):
+        """Connect client to server"""
+        global Is_game_running
         self.client.connect(self.address)
+        Is_game_running = True
+        print("game is running")
         return self.client.recv(2048).decode()
 
     def send(self, data):
+        """Send data to server"""
         try:
+
             self.client.send(str.encode(data))
             reply = self.client.recv(2048).decode()
             return reply
         except socket.error as error:
             return str(error)
 
+# client send receive
+# server send receive
+# create constructor for new class
+# in server receive
+# control from client to server sending function
+# bind server and bind client, not in loop, once
+# -> 1 logic variable to check whether they are binded or not
+
 
 def send_coordinates():
-    """Server send coordinates of players"""
+    """Server send coordinates of players and ball"""
 
 
 def receive_coordinates():
-    """Server receive coordinates of players"""
+    """Server receive coordinates of players and ball"""
+    # receive objects coordinate
 
 
 if __name__ == "__main__":
