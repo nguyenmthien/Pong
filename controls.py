1d2d936a68e5c4e000d22d58d654c2bb152f804f
nguyenmthien <nguyenmthien@live.com>
2020-04-24 01:13:05 +0700

    add select server screen and find server ritual

--- controls.py
+++ controls.py
@@ -1,12 +1,14 @@
 """Input for game
 """
 import sys
+import threading
 import pygame
 import assets
 import networking
 
 
-def game_input(assets_obj: assets.Assets, ui_obj: assets.UserInterface):
+def game_input(assets_obj: assets.Assets,
+               ui_obj: assets.UserInterface):
     """Singleplayer input handler"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,9 +22,7 @@ def game_input(assets_obj: assets.Assets, ui_obj: assets.UserInterface):
             if event.key == pygame.K_ESCAPE:
                 ui_obj.current_menu = "TITLE SCREEN"
                 print("end single player")
-                assets_obj.player.reset()
-                assets_obj.opponent.reset()
-                assets_obj.ball.start()
+                assets_obj.reset()
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
@@ -73,9 +73,7 @@ def local_multiplayer(assets_obj: assets.Assets, ui_obj: assets.UserInterface):
             if event.key == pygame.K_ESCAPE:
                 ui_obj.current_menu = "TITLE SCREEN"
                 print("end local multiplayer")
-                assets_obj.player.reset()
-                assets_obj.opponent.reset()
-                assets_obj.ball.start()
+                assets_obj.reset()
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
@@ -108,20 +106,16 @@ def client(assets_obj: assets.Assets,
             if event.key == pygame.K_s:
                 assets_obj.opponent.speed -= assets_obj.opponent.control_speed
             if event.key == pygame.K_ESCAPE:
-                networking_obj.is_game_running = False
-                networking_obj.network_disconnect()
-                # TODO: add network disconnect
-                ui_obj.current_menu = "TITLE SCREEN"
+                networking_obj.network_disconnect(assets_obj, ui_obj)
                 print("end tcp client")
-                assets_obj.player.reset()
-                assets_obj.opponent.reset()
-                assets_obj.ball.start()
 
         if assets_obj.opponent.speed != assets_obj.opponent.previous_speed:
-            networking_obj.send_controls(assets_obj)
+            networking_obj.send_controls(assets_obj, ui_obj)
 
 
-def wait(assets_obj: assets.Assets, ui_obj: assets.UserInterface):
+def wait_for_client(assets_obj: assets.Assets,
+                    ui_obj: assets.UserInterface,
+                    networking_obj: networking.Networking):
     """waiting screen input handler"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -129,9 +123,64 @@ def wait(assets_obj: assets.Assets, ui_obj: assets.UserInterface):
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                ui_obj.current_menu = "TITLE SCREEN"
                 print("end hosting")
-                assets_obj.player.reset()
-                assets_obj.opponent.reset()
-                assets_obj.ball.start()
-                #TODO: end hosting
+                networking_obj.end_hosting(assets_obj, ui_obj)
+
+
+def choose_server(assets_obj: assets.Assets,
+                  ui_obj: assets.UserInterface,
+                  networking_obj: networking.Networking,
+                  ip_scanner: networking.ScanIP):
+    """Choose server screen input handler"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("exit game")
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if ui_obj.choice > 0:
+                    ui_obj.choice -= 1
+                return
+            if event.key == pygame.K_DOWN:
+                if ui_obj.choice < len(networking_obj.ip_result['found']):
+                    ui_obj.choice += 1
+                return
+            if event.key == pygame.K_RETURN:
+                print(f"Connect to {networking_obj.ip_result['found'][ui_obj.choice]}")
+                networking_obj.connect_to_sever(
+                    networking_obj.ip_result['found'][ui_obj.choice])
+                return
+            if event.key == pygame.K_F5:
+                scan_thr = threading.Thread(target=networking_obj.scan_for_server,
+                                            args=[ip_scanner, ui_obj])
+                scan_thr.start()
+                return
+            if event.key == pygame.K_ESCAPE:
+                networking_obj.network_disconnect(assets_obj, ui_obj)
+                return
+
+
+def server_input(assets_obj: assets.Assets,
+                 ui_obj: assets.UserInterface,
+                 networking_obj: networking.Networking):
+    """Singleplayer input handler"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                assets_obj.player.speed -= assets_obj.player.control_speed
+            if event.key == pygame.K_DOWN:
+                assets_obj.player.speed += assets_obj.player.control_speed
+            if event.key == pygame.K_ESCAPE:
+                print("end hosting")
+                assets_obj.reset()
+                networking_obj.end_hosting(assets_obj, ui_obj)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                assets_obj.player.speed += assets_obj.player.control_speed
+            if event.key == pygame.K_DOWN:
+                assets_obj.player.speed -= assets_obj.player.control_speed
--- networking.py
+++ networking.py
@@ -1,17 +1,32 @@
 """Networking functionalities"""
 import json
 import os
+import select
 import socket
 import subprocess
 import threading
 import assets
 
-LOCAL_IP = socket.gethostbyname(socket.gethostname())  # only known to work on os=NT
+# This is the local IP addr constant. Not tested on os =/= NT
+LOCAL_IP = socket.gethostbyname(socket.gethostname())
+
+# These ports is going to be used by the main server and client in game,
+# but not the only ports used. These ports are choosed to incollide with
+# Sid Meier's Civilization IV network multiplayer ports.
 PORT_SERVER = 2033
 PORT_CLIENT = 2056
 
+# Two German quotes used in the process of validate the legitimate
+# Pong server. The use of German is to avoid false positive.
+# Umlauts are not a problem, since they will be encode with utf-8.
+RITUAL_STR_SERVER = "Jeder nach seinen Fähigkeiten, jedem nach seinen Bedürfnissen!"
+RITUAL_STR_CLIENT = "Proletarier aller Länder, vereinigt Euch!"
+
+
 class ScanIP:
     """Scan IP for ping-able hosts"""
+    # This class is unneccessary now, due to it poor perfomance
+    # TODO: remove this class and add function to list possible ips addr
     def __init__(self):
         self.fnull = open(os.devnull, 'w')
         self.is_on_posix = os.name == 'posix'
@@ -29,9 +44,9 @@ class ScanIP:
                 if i in output:
                     raise Exception()
             return output.split()[3]
-        else:
-            output = subprocess.check_output(['arp', '-a', ip_addr]).decode('ascii')
-            return output.splitlines()[3].split()[1].replace('-', ':')
+
+        output = subprocess.check_output(['arp', '-a', ip_addr]).decode('ascii')
+        return output.splitlines()[3].split()[1].replace('-', ':')
 
     def scan_ip_addr(self, ip_addr):
         """Scan IP address using arp or ping"""
@@ -43,22 +58,22 @@ class ScanIP:
                     if i in output:
                         return False
                 return True
-            else:
-                output = subprocess.check_output(['arp', '-a', ip_addr],
-                                                 stderr=subprocess.STDOUT).decode('ascii')
-                for i in self.known_arp_errors_nt:
-                    if i in output:
-                        return False
-                return True
-        else:
-            if self.is_on_posix:
-                return not subprocess.call(['ping', '-c', '1', '-t', '1', ip_addr],
-                                           stdout=self.fnull,
-                                           stderr=subprocess.STDOUT)
-            else:
-                return not subprocess.call(['ping', '-n', '1', '-w', '1000', ip_addr],
-                                           stdout=self.fnull,
-                                           stderr=subprocess.STDOUT)
+
+            output = subprocess.check_output(['arp', '-a', ip_addr],
+                                             stderr=subprocess.STDOUT).decode('ascii')
+            for i in self.known_arp_errors_nt:
+                if i in output:
+                    return False
+            return True
+
+        if self.is_on_posix:
+            return not subprocess.call(['ping', '-c', '1', '-t', '1', ip_addr],
+                                       stdout=self.fnull,
+                                       stderr=subprocess.STDOUT)
+
+        return not subprocess.call(['ping', '-n', '1', '-w', '10', ip_addr],
+                                   stdout=self.fnull,
+                                   stderr=subprocess.STDOUT)
 
     def ip_thread(self, ip_addr, ips):
         """Thread for multithreading"""
@@ -79,7 +94,7 @@ class ScanIP:
     def get_ip_base(self):
         """Find IP base for scanning"""
         ip_base = []
-        if len(ip_base) == 0:
+        if not ip_base:
             if self.is_on_posix:
                 broadcast = []
                 ifconfig = subprocess.check_output('ifconfig').decode('ascii').splitlines()
@@ -99,6 +114,7 @@ class ScanIP:
         """Return list of available [IP, hostname, (MAC)]"""
         all_threads = []
         accepted_ips = []
+        socket.setdefaulttimeout(0.01)
         for ip_addr in ips:
             p_1, p_2, p_3, p_4 = [[int(y) for y in x.split('-')] for x in ip_addr.split('.')]
             for p_i in (p_1, p_2, p_3, p_4):
@@ -128,13 +144,24 @@ class ScanIP:
 class Networking:
     """Networking default class"""
     def __init__(self):
-        self.reply = ""
-        self.is_game_running = False
-        self.is_binded = False
+        # sockets and tools
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket = None
         self.client_address = None
-        self.server_list = []
+        self.socket_list = [self.socket]
+        # result container
+        self.ip_result = {
+            "timeout": [],
+            "found":   [],
+            "invalid": [],
+            "notfound":[],
+        }
+        # progress flags
+        self.flag = {
+            "is_game_running" : False,
+            "is_binded"       : False,
+            "is_scanning"     : False,
+        }
 
     def init_server(self):
         """Initialize socket in server mode"""
@@ -143,108 +170,231 @@ class Networking:
         except (ConnectionRefusedError, OSError):
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.bind((LOCAL_IP, PORT_SERVER))
-        self.is_binded = True
+        self.flag["is_binded"] = True
+        self.socket.setblocking(0)
+        self.socket_list = [self.socket]
         print(f"Binded a TCP socket to {LOCAL_IP}:{PORT_SERVER}")
         self.socket.listen()
         print("waiting for connection")
 
     def wait_for_client(self):
         """Wait and confirm the client"""
-        # TODO: change to fit with client ritual
-        #       use select
-        self.client_socket, self.client_address = self.socket.accept()
-        self.client_socket.settimeout(0.5/assets.FPS)
-        print(f"Conneted to client at {self.client_address}")
-        print(self.client_socket)
-        self.is_game_running = True
+        # We use select to categorize ready to read sockets form socket_list, then
+        # loop through every one of them to perform the server connect ritual.
+        # This instruction is blocking, consider an unblocking solution using threading
+        ready_to_read, _, _ = select.select(self.socket_list, [], [])
+        for notified_socket in ready_to_read:
+
+            # The server connect ritual is to send RITUAL_STR_SERVER for every new connection
+            # then wait for the return string. The game will start if the client send
+            # RITUAL_STR_CLIENT, and the server will disconnect with the client if the client
+            # send None
+
+            # If our server socket is ready to read, that's mean
+            # there is a new connection.
+
+            if notified_socket == self.socket:
+                self.client_socket, self.client_address = self.socket.accept()
+                self.client_socket.settimeout(10/assets.FPS)
+                self.client_socket.send(RITUAL_STR_SERVER.encode('utf-8'))
+                return_str = self.client_socket.recv(1024).decode('utf-8')
+                if return_str == RITUAL_STR_CLIENT:
+                    print(f"Conneted to client at {self.client_address}")
+                    print(self.client_socket)
+                    self.client_socket.settimeout(0.5/assets.FPS)
+                    self.flag["is_game_running"] = True
+                    return
+                if return_str is None:
+                    print(f"Detected client at {self.client_address}")
+                    return
 
     def init_client(self):
         """Initialize socket in client mode"""
-        self.socket.bind((LOCAL_IP, PORT_CLIENT))
-        self.is_binded = True
+        try:
+            self.socket.bind((LOCAL_IP, PORT_CLIENT))
+        except OSError:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind((LOCAL_IP, PORT_CLIENT))
+        finally:
+            self.socket.settimeout(10/assets.FPS)
+            self.flag["is_binded"] = True
         print(f"Binded a TCP socket to {LOCAL_IP}:{PORT_CLIENT}")
 
-    def scan_for_server(self, ui_obj: assets.UserInterface, ip_scanner: ScanIP):
+    def scan_for_server(self, ip_scanner: ScanIP, ui_obj: assets.UserInterface):
         """Scan for available IPs and pass it to self.server_list"""
-        ui_obj.server_ip_list = []
-        host_list = ip_scanner.main(ip_scanner.get_ip_base())
-        for ip_list in host_list:
-            if self.find_server_ritual(ip_list[0]):
-                ui_obj.server_ip_list.append(ip_list[0])
+
+        # We use the IP scanner to find the list of possible host,
+        # then loop through every one of them to find the pong host.
+        # Note that this config use threading to speed up the search
+        # process, hence we will create a lot of throwaway socket
+        # in the loop
+
+        self.flag['is_scanning'] = True
+        self.ip_result['timeout'] = []
+        self.ip_result['invalid'] = []
+        self.ip_result['notfound'] = []
+        self.ip_result['found'] = []
+
+        ui_obj.choice = 0
+
+        host_list = ip_scanner.get_ip_base()
+        print(f"Found these hosts on the local network:")
+        print(host_list)
+        threads = []
+        for ip_addr in host_list:
+            # Due to the format of the ip_base: [1.2.3.1-254], we now split the addr to four
+            # lists p_1 to p_4, by ".", and the indexes in each p by "-" for us to loop.
+            # Remember to change this when the search funtion is done.
+
+            p_1, p_2, p_3, p_4 = [[int(y) for y in x.split('-')] for x in ip_addr.split('.')]
+            for p_i in (p_1, p_2, p_3, p_4):
+                if len(p_i) == 1:
+                    p_i.append(p_i[0]+1)
+                else:
+                    p_i[1] += 1
+
+            for i_1 in range(p_1[0], p_1[1]):
+                for i_2 in range(p_2[0], p_2[1]):
+                    for i_3 in range(p_3[0], p_3[1]):
+                        for i_4 in range(p_4[0], p_4[1]):
+                            thrd = threading.Thread(target=self.find_server_ritual,
+                                                    args=[f"{i_1}.{i_2}.{i_3}.{i_4}"])
+                            thrd.start()
+                            threads.append(thrd)
+
+        for thread in threads:
+            thread.join()
+        print(f"Scan results (timeout IPs are ommited):")
+        #print(f"    Timeout: {self.ip_result['timeout']}")
+        print(f"    Not found: {self.ip_result['notfound']}")
+        print(f"    Invalid: {self.ip_result['invalid']}")
+        print(f"    Found: {self.ip_result['found']}")
+        self.flag['is_scanning'] = False
+        return
 
     def find_server_ritual(self, ip_addr):
         """Ritual to find if the host is a legitimate pong server"""
-        # TODO: add find server ritual
-        return True
+
+        # The client ritual is to try to connect to the server, then
+        # recieve a package. If the package is RITUAL_STR_SERVER
+        # then the ip_addr is valid. If not, or we cannot connect, then the
+        # ip_addr is invalid
+
+        try:
+            scan_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            scan_sock.settimeout(2)
+            scan_sock.connect((ip_addr, PORT_SERVER))
+
+        except ConnectionRefusedError:
+            self.ip_result['notfound'].append(ip_addr)
+            return
+
+        except socket.timeout:
+            self.ip_result['timeout'].append(ip_addr)
+            return
+
+        else:
+            ritual = scan_sock.recv(1024).decode('utf-8')
+            scan_sock.close()
+
+            if ritual == RITUAL_STR_SERVER:
+                self.ip_result['found'].append(ip_addr)
+                return
+
+            self.ip_result['invalid'].append(ip_addr)
+            return
 
     def connect_to_sever(self, ip_address):
         """Connect client to server, given IP address"""
-        self.socket.connect((ip_address, PORT_SERVER))
-        print(f"Conneted to server at {ip_address}:{PORT_SERVER}")
-        self.is_game_running = True
+        try:
+            self.socket.connect((ip_address, PORT_SERVER))
+        except OSError:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind((LOCAL_IP, PORT_CLIENT))
+            self.socket.connect((ip_address, PORT_SERVER))
+        finally:
+            self.socket.recv(1024)
+            self.socket.send(RITUAL_STR_CLIENT.encode('utf-8'))
+            print(f"Conneted to server at {ip_address}:{PORT_SERVER}")
+            self.flag['is_game_running'] = True
 
-    def send_coordinates(self, assets_obj: assets.Assets):
+    def send_coordinates(self, assets_obj: assets.Assets, ui_obj: assets.UserInterface):
         """Send coordinates from assets_obj to client"""
         binary = dict_to_binary(assets_obj.get_coordinates())
         try:
             self.client_socket.send(str.encode(binary))
         except AttributeError as msg:
             print(msg)
+        except (socket.timeout, ConnectionAbortedError):
+            self.end_hosting(assets_obj, ui_obj)
+            assets_obj.reset()
 
-    def receive_coordinates(self, assets_obj: assets.Assets):
+    def receive_coordinates(self, assets_obj: assets.Assets, ui_obj: assets.UserInterface):
         """Use in client, recive data from server and decode it"""
         try:
             binary = self.socket.recv(2048)  # allow receiving 2048 bits data
-            binary_decoded = binary.decode("utf-8")  # utf-8 encoding
             if not binary:  # if sending incompleted data
                 print("disconnected")
-                self.network_disconnect()
-            else:
-                translated_binary = binary_to_dict(binary_decoded)
-                # print(f"Recieved: {translated_binary}")
-                if translated_binary:
-                    assets_obj.set_coordinates(translated_binary)
+                self.network_disconnect(assets_obj, ui_obj)
+                return
+            binary_decoded = binary.decode("utf-8")  # utf-8 encoding
+            translated_binary = binary_to_dict(binary_decoded)
+            # print(f"Recieved: {translated_binary}")
+            if translated_binary:
+                assets_obj.set_coordinates(translated_binary)
         except OSError as msg:
-            self.network_disconnect()
+            self.network_disconnect(assets_obj, ui_obj)
+            print(msg)
+        except socket.timeout as msg:
+            self.network_disconnect(assets_obj, ui_obj)
             print(msg)
 
-    def send_controls(self, assets_obj: assets.Assets):
+    def send_controls(self, assets_obj: assets.Assets, ui_obj: assets.UserInterface):
         """Use in client, send control to server"""
         control = assets_obj.get_opponent_speed()
-        self.socket.send(str(control).encode('utf-8'))
+        try:
+            self.socket.send(str(control).encode('utf-8'))
+        except ConnectionAbortedError:
+            self.network_disconnect(assets_obj, ui_obj)
 
-    def recieve_controls(self, assets_obj: assets.Assets):
+    def recieve_controls(self, assets_obj: assets.Assets, ui_obj: assets.UserInterface):
         """Use in server, recieve control from client"""
         # TODO: change to select
         #       add logic branch to avoid set speed to 66 or -66
         try:
-            control = self.client_socket.recv(5)
+            control = self.client_socket.recv(10)
             control = control.decode('utf-8')
-            if control is False:  # if sending incompleted data
+            print(f"Recieved: {control}")
+            if control is None:  # if sending incompleted data
                 print("disconnected")
-                self.network_disconnect()
+                self.network_disconnect(assets_obj, ui_obj)
             else:
                 assets_obj.set_opponent_speed(int(control))
-                print(f"Recieved: {control}")
-        except socket.timeout as msg:
-            print(msg)
+        except socket.timeout:
+            pass
         except ValueError as msg:
             print(msg)
         except OSError as msg:
             print(msg)
 
-    def end_hosting(self):
-        self.is_binded = False
-        self.is_game_running = False
+    def end_hosting(self, assets_obj: assets.Assets, ui_obj: assets.UserInterface):
+        """Use in server to end multiplayer mode"""
+        self.flag['is_binded'] = False
+        self.flag['is_game_running'] = False
+        assets_obj.reset()
+        ui_obj.current_menu = "TITLE SCREEN"
         try:
             self.socket.shutdown(socket.SHUT_RDWR)
         except (socket.error, OSError, ValueError) as msg:
             print(msg)
         self.socket.close()
 
-    def network_disconnect(self):
-        self.is_binded = False
-        self.is_game_running = False
+    def network_disconnect(self, assets_obj: assets.Assets, ui_obj: assets.UserInterface):
+        """Use in client to end multiplayer mode"""
+        self.flag['is_binded'] = False
+        self.flag['is_game_running'] = False
+        assets_obj.reset()
+        ui_obj.current_menu = "TITLE SCREEN"
         try:
             self.socket.close()
         except (socket.error, OSError, ValueError) as msg:
@@ -268,21 +418,33 @@ def dict_to_binary(dictionary):
 
 
 if __name__ == "__main__":
+    # all of these is only used for testing only.
     import time
-
+    import pygame
+    RUNNING = False
+    def ui_thread():
+        """Thread for threading"""
+        while RUNNING:
+            UI.wait_for_search(ASSETS)
+            pygame.event.get()
+    def net_thread():
+        """Thread for threading"""
+        NET.scan_for_server(SCAN_IP, UI)
+    RUNNING = True
     SCAN_IP = ScanIP()
-    T3 = time.time()
-    print(SCAN_IP.main(SCAN_IP.get_ip_base()))
-    T4 = time.time()
-    print(T4-T3)
-
     ASSETS = assets.Assets()
     UI = assets.UserInterface()
     NET = Networking()
-    UI.choose_server(ASSETS)
+    UI_THR = threading.Thread(target=ui_thread, args=[])
+    NET_THR = threading.Thread(target=net_thread, args=[])
+    print(f"created threads successfully!")
     T1 = time.time()
-    NET.scan_for_server(UI, SCAN_IP)
+    UI_THR.start()
+    NET_THR.start()
+    NET_THR.join()
+    RUNNING = False
+    UI_THR.join()
     T2 = time.time()
-    UI.choose_server(ASSETS)
-    print(T2-T1)
+    UI.choose_server(ASSETS, NET.ip_result['found'])
+    print(f"Scanning time: {T2-T1} s")
     time.sleep(5)
