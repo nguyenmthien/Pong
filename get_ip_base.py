742207c265f561c78e46d10c77d208a2b1d4bb1a
nguyenmthien <nguyenmthien@live.com>
2020-04-25 23:15:16 +0700

    change to enumerate

--- get_ip_base.py
+++ get_ip_base.py
@@ -3,13 +3,8 @@ import os
 import subprocess
 import ipaddress
 
-FNULL = open(os.devnull, 'w')
 IS_ON_POSIX = os.name == 'posix'
-SHOW_MAC = False
-USE_ARP = False
 
-KNOWN_ARP_ERRORS_POSIX = ['-- no entry', '(incomplete)']
-KNOWN_ARP_ERRORS_NT = ['No ARP Entries Found.']
 
 def ip2bin(ip_addr):
     """Convert IP address to binary"""
@@ -19,7 +14,7 @@ def ip2bin(ip_addr):
     return binary[:range_bin] if range_bin else binary
 
 def get_prefix(binary):
-    """Get the Prefix of Subnet Mask"""
+    """Get the prefix of Subnet Mask"""
     prefix_count = 0
     for i in binary:
         if i == '1':
@@ -39,6 +34,9 @@ def get_ip_base():
                 if 'broadcast' in i:
                     broadcast = i.split('broadcast')[1].strip()
                     ip_addr.append('.'.join(broadcast.split('.')[:-1]) + '.0')
+                if 'Mask' in i:
+                    box = i.split()[-1].strip()
+                    subnet_mask.append(box)
         else:
             ipconfig = subprocess.check_output('ipconfig').decode('ascii').splitlines()
             for i in ipconfig:
@@ -50,20 +48,20 @@ def get_ip_base():
                     subnet_mask.append(box)
 
     ip_base = []
-    for i in range(len(subnet_mask)):
-        prefixlen = str(get_prefix(ip2bin(subnet_mask[i])))
+    for i, subnet in enumerate(subnet_mask):
+        prefixlen = str(get_prefix(ip2bin(subnet)))
         ip_add = ip_addr[i]
         ip_base.append(ip_add + '/' + prefixlen)
 
     j = 0
     list_ip = []
-    for i in range(len(ip_base)):
+    for i, _ in enumerate(ip_base):
         list_ip.append(list(ipaddress.ip_network(ip_base[j]).hosts()))
         j = j+1
 
-    for i in range(len(list_ip)):                   #Convert ip in list_ip to string
-        for j in range(len(list_ip[i])):
-            list_ip[i][j] = str(list_ip[i][j])
+    for i, ip_list in enumerate(list_ip):                   #Convert ip in list_ip to string
+        for j, addr in enumerate(ip_list):
+            list_ip[i][j] = str(addr)
 
     return list_ip
 
